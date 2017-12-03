5c1d22db44abd1f2ec268630e975ca4973335d2e
Rashmita.Mishra <rashmita.mishra@gmail.com>
2017-12-03 10:41:26 +0530

    gt

---
+++ BiggestString.java
@@ -0,0 +1,33 @@
+/* class BiggestString
+{
+	static String findBiggestString(String S,String K)
+	{
+		int i=S.length();
+		int j=K.length();
+		if(i>j)
+		{
+			return S;
+		}
+		else
+		{
+			return K;
+		}
+	}
+}   or */ 
+
+class BiggestString
+{	
+	static String findBiggestString(String S,String K)
+	{
+		int maxlen=Integer.max(S.length(),K.length());
+		if(S.length()==maxlen)
+		{
+		return S;
+		}
+		else
+		{
+		return K;
+		}
+	}
+
+}
---
+++ findPosition.java
@@ -0,0 +1,11 @@
+class Position
+{
+	public static void main(String... ab)
+	{
+		String S="Rashmi";
+		char p='i';
+		
+		System.out.println(S.indexOf(p));
+	}
+}
+
